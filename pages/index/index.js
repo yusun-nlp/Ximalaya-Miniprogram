b6dcd29e59f12d620007d3ea34af2c995a4bd7f9
JuliaSun623 <cnsdytsy@gmail.com>
2020-09-26 18:38:38 +0800

    finish index page

---
+++ pages/index/index.js
@@ -0,0 +1,93 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var utils = require('../../utils/util.js');
+
+Page({
+  data: {
+    imgList: [
+      '/image/ad1.jpg',
+      '/image/ad2.jpg',
+      '/image/ad3.jpg',
+      '/image/ad4.jpg',
+      '/image/ad5.jpg',
+      '/image/ad6.jpg',
+      '/image/ad7.jpg'
+    ],
+    navList: [{
+        icon: '/image/nav-icon/jingdian.png',
+        evenets: 'goToBangDan',
+        text: '经典必听'
+      },
+      {
+        icon: '/image/nav-icon/meiri.png',
+        evenets: 'goToBangDan',
+        text: '每日听单'
+      },
+      {
+        icon: '/image/nav-icon/qinggan.png',
+        evenets: 'goToBangDan',
+        text: '情感电台'
+      },
+      {
+        icon: '/image/nav-icon/zhumian.png',
+        evenets: 'goToBangDan',
+        text: '助眠解压'
+      },
+      {
+        icon: '/image/nav-icon/zhibo.png',
+        evenets: 'goToBangDan',
+        text: '直播交友'
+      },
+    ],
+    swiperCurrent: 0,
+  },
+
+  /**
+   * 生命周期函数 —— 监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    var url = 'https://mobile.ximalaya.com/mobile/discovery/v3/recommend/hotAndGuess?code=43_310000_3100&device=android&version=5.4.45';
+
+    // 调用自己封装的工具函数，在utils中
+    utils.myRequest({
+      url: url,
+      methods: 'GET',
+      success: function(result){
+        that.setData({
+          showitem: true,
+          guess: result.data.paidArea.list,
+          xiaoshuocontent: result.data.hotRecommends.list[0].list,
+          xiangshengcontent: result.data.hotRecommends.list[2].list,
+          lishicontent: result.data.hotRecommends.list[3].list
+        })
+      },
+      fail: function() {
+        that.setData({
+          showitem: false
+        })
+      }
+    });
+  },
+  // 轮播图改变事件
+  swiperChange: function(e) {
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
+  // 宫格导航改变事件
+  goToBangDan: function() {
+    wx.navigateTo({
+      url: '/pages/index/bangdan/bangdan',
+    })
+  },
+  // 猜你喜欢改变事件
+  gotoDetails(e) {
+    var url = e.currentTarget.dataset.coverimg;
+    var title = e.currentTarget.dataset.title;
+    wx.navigateTo({
+      url: '/pages/details/details?url=' + url + '&title=' + title,
+    })
+  }
+})
---
+++ utils/util.js
@@ -0,0 +1,30 @@
+/*
+  基于wx.request封装的一个请求函数
+  因为在小程序开发中request是最常用的api所以会造成很多的代码重复
+  因此将其在封装之后可以大大的减少代码的复用
+*/
+
+let myRequest = function(args = {url:'', methods:'GET', data:{}, success:function(){}, fail:function(){}}){
+  wx.request({
+    url: args.url,
+    data: args.data,
+    header: {'content-type': 'application/json'},
+    method: args.methods,
+    dataType: 'json',
+    responseType: 'text',
+    success: (res)=>{
+      console.log(res);
+      if(res.statusCode == 200){  //请求成功执行回调函数
+        args.success(res)
+      }
+      else{ //请求失败执行回调函数
+        args.fail()
+      }
+    },
+  })
+}
+
+// 向外暴露接口
+module.exports = {
+  myRequest: myRequest
+}
