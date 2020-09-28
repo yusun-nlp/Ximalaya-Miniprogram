b2d7e21d9a7536fc7fd2f5f9819f1d71a7ccbfbd
JuliaSun623 <cnsdytsy@gmail.com>
2020-09-28 19:36:17 +0800

    add login & self-page

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 
 | 日期 | 实现页面 | 实现功能                                   | 界面展示                                                     |
 | ---- | -------- | ------------------------------------------ | ------------------------------------------------------------ |
-| 9.26 | index    | 轮播图、导航格、猜你喜欢、部分模块精选展示 | <img src="README.assets/1.jpg" alt="1" style="zoom:50%;" /> <img src="README.assets/2.png" alt="2" style="zoom:50%;" /> |
-| 9.27 | bangdan  | 分类榜单、下拉列表                         | <img src="README.assets/4.jpg" alt="4" style="zoom:50%;" /><img src="README.assets/3.jpg" alt="3" style="zoom:50%;" /> |
-|      |          |                                            |                                                              |
+| 9.26 | index    | 轮播图、导航格、猜你喜欢、部分模块精选展示 | <img src="README.assets/1.jpg" alt="1" style="zoom:33%;" /> <img src="README.assets/2.png" alt="2" style="zoom:33%;" /> |
+| 9.27 | bangdan  | 分类榜单、下拉列表                         | <img src="README.assets/4.jpg" alt="4" style="zoom:33%;" /><img src="README.assets/3.jpg" alt="3" style="zoom:33%;" /> |
+| 9.28 | user     | 登录、用户界面                             | <img src="README.assets/image-20200928162458144.png" alt="image-20200928162458144" style="zoom:33%;" />   <img src="README.assets/image-20200928192955222.png" alt="image-20200928192955222" style="zoom:33%;" />  <img src="README.assets/image-20200928193038506.png" alt="image-20200928193038506" style="zoom:33%;" /> |
 
 
 
---
+++ pages/user/user.js
@@ -0,0 +1,95 @@
+const app = getApp()
+let userInfo = app.globalData.userInfo;
+
+Page({
+  data:{
+    timeout:[
+      {text: "不开启"},
+      {text: "播放完当前声音关闭"},
+      {text: "播放2首声音关闭"},
+      {text: "播放3首声音关闭"},
+      {text: "10分钟后关闭"},
+      {text: "30分钟后关闭"},
+      {text: "1小时后关闭"},
+    ],
+    activeIndex: 0,
+  },
+
+  onLoad: function(options){
+    var that = this;
+    //获得设备信息
+    wx.getSystemInfo({
+      success (res) {
+        console.log(res.windowHeight);
+        that.setData({
+          phoneHeight: res.windowHeight,
+        })
+      }
+    })
+    //查看是否授权
+    if(app.globalData.userInfo == null){
+      that.setData({
+        login: true
+      })
+    }else{
+      that.setData({
+        login: false,
+        avatarUrl: app.globalData.userInfo.avatarUrl,
+        nickName: app.globalData.userInfo.nickName
+      })
+    }
+  },
+
+  //获取用户的头像和昵称信息
+  bindGetUserInfo(e){
+    var that = this;
+    wx.getUserInfo({
+      success: function(res){
+        console.log(e.detail.userInfo);
+        app.globalData.userInfo = e.detail.userInfo;
+        that.setData({
+          login: false,
+          avatarUrl: e.detail.userInfo.avatarUrl,
+          nickName: e.detail.userInfo.nickName
+        })
+      }
+    })
+  },
+
+  //手机登录
+  phoneLogin: function(){
+    wx.navigateTo({
+      url: './phoneLogin/phoneLogin',
+    });
+  },
+
+  //定时关闭
+  openSwitch: function(){
+    var that = this;
+    that.setData({
+      show: true
+    })
+  },
+
+  //切换账号
+  gtoLogin(){
+    wx.navigateTo({
+      url: './phoneLogin/phoneLogin',
+    });
+  },
+
+  close: function(){
+    var that = this;
+    that.setData({
+      show: false
+    })
+  },
+
+  chooseTimeOut:function(e){
+    var that = this;
+    console.log(e)
+    that.setData({
+      activeIndex: e.currentTarget.dataset.activeIndex
+    })
+  }
+})
